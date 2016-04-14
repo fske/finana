746a66238c9fff3aafdbb61a65c6bd461101a0df
fske <fusion.ker@live.com>
2016-04-14 22:50:21 +0800

    add headers

--- get_precious_metal_price.py
+++ get_precious_metal_price.py
@@ -9,12 +9,13 @@ conn = psycopg2.connect(database="test", user="root", password="d&kdz555", host=
 cur = conn.cursor()
 cur.execute("SELECT MAX(id)+1 FROM price_precious_metal")
 id = cur.fetchall()[0][0] or 1
+url = "http://m.kitco.com"
+headers = [{'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20151201 Firefox/3.5.6' }, {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}]
 
 while True :
     timer = int(time.time()) 
     if timer % 60 == 0 :
-        url = "http://m.kitco.com"
-        request = urllib2.Request(url)
+        request = urllib2.Request(url = url, headers = headers[(timer%13)%2])
         wp = urllib2.urlopen(request)
         content = wp.read().replace("\r","").replace("\n", "").replace("\t","")
         price_list = []
