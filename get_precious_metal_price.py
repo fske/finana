fa685132aca1f8842f91166e162447cee789e359
fske <fusion.ker@live.com>
2016-04-14 22:20:05 +0800

    charge get method of webpage

--- get_precious_metal_price.py
+++ get_precious_metal_price.py
@@ -14,27 +14,18 @@ while True :
     timer = int(time.time()) 
     if timer % 60 == 0 :
         url = "http://m.kitco.com"
-        wp = urllib2.urlopen(url)
+        request = urllib2.Request(url)
+        wp = urllib2.urlopen(request)
         content = wp.read().replace("\r","").replace("\n", "").replace("\t","")
-#        fp = open("/root/collection/web2.txt","w+")
-#        fp.write(content)
-#        fp.close()
-#        print "target website: " + wp.geturl()
         price_list = []
         for target in target_list:
             regexp = target + r'</a></td>\s*<td\s*nowrap=\"nowrap\">[0-9]+\.[0-9]+</td>'
-#            print regexp
             pattern = re.compile(r'' + regexp + '')
             matched = pattern.search(content)
-            #print matched
             price = re.search(r"[0-9]+\.[0-9]+", matched.group(0))
             price_list.append(str(float(price.group(0)) * 100))
         record = str(timer) + "," + ",".join(price_list)
-#        fp = open("/root/collection/pmetal_price.dat","a+")
-#        fp.write(record)
-#        fp.close()
         sql = "INSERT INTO price_precious_metal (id,bid_time,gold_price,silver_price,platinum_price) VALUES (" + str(id) + "," + record + ")"
-        print sql
         cur.execute(sql)
         conn.commit()
         id = id + 1
